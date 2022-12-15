5eaab755bfcb4e5005e730aeb233ae6f1fbbabdf
Sergei Surovtsev <97428129+stillonearth@users.noreply.github.com>
2022-12-15 11:46:18 +0300

    npm.js package

---
+++ index.js
@@ -0,0 +1,12 @@
+import { Buffer } from "buffer";
+import { mujoco_wasm } from "./publuc/mujoco-wasm";
+
+export async function downloadFile(url, outputPath) {
+    const x = await fetch(url);
+    const x_1 = await x.arrayBuffer();
+    FS.writeFile(outputPath, Buffer.from(x_1));
+
+}
+
+export * from mujoco_wasm;
+
