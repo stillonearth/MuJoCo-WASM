e5ecb7a531747dcd64be7b3501767adaee467da4
Johnathon Selstad <makeshifted@gmail.com>
2023-03-16 14:19:19 -0700

    Modularize!

---
+++ make_wasm.bat
@@ -0,0 +1,9 @@
+rmdir /s /q build
+call ../emsdk/emsdk activate latest
+mkdir build
+cd build
+call emcmake cmake ..
+call emmake make
+cd ../public
+rem call npx tsembind mujoco_wasm.js > mujoco_wasm.d.ts
+pause
