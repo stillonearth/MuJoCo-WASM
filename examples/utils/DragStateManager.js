8c404d89be754e3ea8f440dd3f0da72df3c6ee07
Kevin Zakka <kevinarmandzakka@gmail.com>
2023-03-22 23:11:16 -0700

    Add ability to highlight object on double click.

--- examples/utils/DragStateManager.js
+++ examples/utils/DragStateManager.js
@@ -26,6 +26,9 @@ export class DragStateManager {
         this.arrow.cone.material.opacity = 0.5;
         this.arrow.visible = false;
 
+        this.previouslySelected = null;
+        this.higlightColor = 0xff0000;  // 0x777777
+
         this.localHit = new Vector3();
         this.worldHit = new Vector3();
         this.currentWorld = new Vector3();
@@ -34,6 +37,7 @@ export class DragStateManager {
         document.addEventListener( 'pointermove', this.onPointer.bind(this), true );
         document.addEventListener( 'pointerup'  , this.onPointer.bind(this), true );
         document.addEventListener( 'pointerout' , this.onPointer.bind(this), true );
+        container.addEventListener( 'dblclick', this.onPointer.bind(this), false );
     }
     updateRaycaster(x, y) {
         var rect = this.renderer.domElement.getBoundingClientRect();
@@ -105,6 +109,27 @@ export class DragStateManager {
             if (this.active) { this.move(evt.clientX, evt.clientY); }
         } else if (evt.type == "pointerup" /*|| evt.type == "pointerout"*/) {
             this.end(evt);
+        }
+        if (evt.type == "dblclick") {
+            this.start(evt.clientX, evt.clientY);
+            this.doubleClick = true;
+            if (this.physicsObject) {
+                if (this.physicsObject == this.previouslySelected) {
+                    this.physicsObject.material.emissive.setHex(0x000000);
+                    this.previouslySelected = null;
+                } else {
+                    if (this.previouslySelected) {
+                        this.previouslySelected.material.emissive.setHex(0x000000);
+                    }
+                    this.physicsObject.material.emissive.setHex(this.higlightColor);
+                    this.previouslySelected = this.physicsObject;
+                }
+            } else {
+                if (this.previouslySelected) {
+                    this.previouslySelected.material.emissive.setHex(0x000000);
+                    this.previouslySelected = null;
+                }
+            }
         }
     }
 }
